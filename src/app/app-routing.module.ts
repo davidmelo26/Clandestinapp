88bb6cdf5c633a04d8ab5cd65d1b58f3632bc330
David Melo <30818594+davidmelo26@users.noreply.github.com>
2020-11-21 20:42:02 -0500

    Rutas para HomePage y AprendaPage

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { AprendaPageComponent } from './componentes/aprenda-page/aprenda-page.component'; 
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
+
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent},
+  { path: 'aprenda', component: AprendaPageComponent},
+  { path: '**', component: NotFoundPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoginPageComponent } from './componentes/login-page/login-page.componen
 import { AprendaPageComponent } from './componentes/aprenda-page/aprenda-page.component';
 import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
